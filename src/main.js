9df9fb34e24ed5af417c2b5bab617a0eec525dea
Daniel Tomlinson <dtomlinson@panaetius.co.uk>
2020-05-29 03:58:33 +0100

    Updating latest

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-import './scss/styles.scss';
+import "./scss/styles.scss";
+import(/* webpackChunkName: "bootstrap" */ "bootstrap");
+
+window.addEventListener("DOMContentLoaded", async (event) => {
+  const { default: App } = await import(
+    /* webpackChunkName: "app" */ "./js/App"
+  );
+  App.bootstrapify();
+  App.lazyload();
+  App.loadFontAwesome();
+  App.navbarFade();
+});
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,11 @@ const webpack = require("webpack");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizer = require("optimize-css-assets-webpack-plugin");
+const PurgeCssPlugin = require("purgecss-webpack-plugin");
+const glob = require("glob");
+const path = require("path")
 
 module.exports = merge(common, {
   mode: "production",
@@ -32,5 +37,17 @@ module.exports = merge(common, {
       sourceMap: true,
     }),
     new webpack.HashedModuleIdsPlugin(),
+    new PurgeCssPlugin({
+      paths: glob.sync(path.join(__dirname, "layouts") + "/**/*.html", {
+        nodir: true,
+      }),
+    }),
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({ extractComments: false }),
+      new CssMinimizer(),
+    ],
+  },
 });
