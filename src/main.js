0b8b8c1d7ee95ec17db1d523237b509c0df45c37
Daniel Tomlinson <dtomlinson@panaetius.co.uk>
2020-07-22 20:54:19 +0100

    Adding webpackChunkName

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ window.addEventListener("DOMContentLoaded", async (event) => {
   const { default: App } = await import(
     /* webpackChunkName: "app" */ "./js/App"
   );
-  const animatedToc = await import("./js/toc");
+  const animatedToc = await import(/* webpackChunkName: "toc" */ "./js/toc");
   App.bootstrapify();
   App.lazyload();
   App.loadFontAwesome();
