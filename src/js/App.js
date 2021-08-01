f0b248ffb58d8f28b81e699ec9eab9bc93aa4d45
Daniel Tomlinson <dtomlinson@panaetius.co.uk>
2021-08-01 04:43:29 +0100

    adding encrypted App.js

---
+++ src/js/App.js
@@ -0,0 +1,133 @@
+"use strict";
+import AOS from "aos";
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import docsearch from "docsearch.js";
+
+import { faBookOpen, faChevronLeft, faChevronRight, faCircle, faClock, faEnvelope, faRss, faTag, faSearch, faFileCode } from "@fortawesome/free-solid-svg-icons";
+
+import {
+  faFacebook,
+  faFacebookF,
+  faGithub,
+  faGitlab,
+  faInstagram,
+  faKeybase,
+  faLinkedin,
+  faLinkedinIn,
+  faMastodon,
+  faMedium,
+  faPinterest,
+  faReddit,
+  faRedditAlien,
+  faStackOverflow,
+  faTwitter,
+  faWeibo,
+} from "@fortawesome/free-brands-svg-icons";
+
+library.add(
+  faBookOpen,
+  faChevronLeft,
+  faChevronRight,
+  faCircle,
+  faClock,
+  faEnvelope,
+  faFacebook,
+  faFacebookF,
+  faGithub,
+  faGitlab,
+  faInstagram,
+  faKeybase,
+  faLinkedin,
+  faLinkedinIn,
+  faMastodon,
+  faMedium,
+  faPinterest,
+  faReddit,
+  faRedditAlien,
+  faRss,
+  faStackOverflow,
+  faTag,
+  faTwitter,
+  faWeibo,
+  faSearch,
+  faFileCode
+);
+
+export default {
+  loadFontAwesome: () => {
+    dom.watch();
+  },
+  bootstrapify: () => {
+    $(".content blockquote").addClass("blockquote");
+    $(".content table").addClass("table");
+    $(".content table").wrap('<div class="table-responsive" />');
+    $(".content table thead").addClass("thead-dark");
+    $(".content figure > img").addClass("img-fluid");
+  },
+  lazyload: async () => {
+    const { default: LazyLoad } = await import(/* webpackChunkName: "lazyload" */ "vanilla-lazyload");
+    new LazyLoad({
+      thresholds: "40px 10%",
+      load_delay: 100,
+    });
+  },
+  // navbarFade: () => {
+  //   let $position = $(window).scrollTop();
+
+  //   $(window).scroll(() => {
+  //     const $scroll = $(window).scrollTop();
+  //     const $navbarHeight = $("#navbar-main-menu.fixed-top").outerHeight();
+
+  //     $scroll > $position ? $("#navbar-main-menu.fixed-top").css("top", -$navbarHeight) : $("#navbar-main-menu.fixed-top").css("top", 0);
+
+  //     if ($scroll <= 0) {
+  //       $("#navbar-main-menu.fixed-top").css("top", 0);
+  //     }
+
+  //     $position = $scroll;
+  //   });
+  // },
+  aos: () => {
+    AOS.init({ duration: 1000, once: false, useClassNames: true });
+  },
+  scrollBar: () => {
+    $(window).on("load scroll resize", function () {
+      let top = $(window).scrollTop();
+      let height = $(document).height();
+      let windowheight = $(window).height();
+      let width = (top / (height - windowheight)) * 100;
+      $("#line-scroll").css({
+        width: width + "%",
+      });
+    });
+  },
+  test: () => {
+    console.log("testing if webpack loads this on start, or for every page?");
+  },
+  tocbot: () => {
+    console.log("tocbot initalised");
+    var tocbot = require("tocbot");
+    tocbot.init({
+      // Where to render the table of contents.
+      tocSelector: ".js-toc",
+      // Where to grab the headings to build the table of contents.
+      contentSelector: ".js-toc-content",
+      // Which headings to grab inside of the contentSelector element.
+      headingSelector: "h1, h2, h3",
+      // For headings inside relative or absolute positioned containers within content.
+      hasInnerContainers: true,
+      // Offset so the title is shown.
+      headingsOffset: 30,
+    });
+  },
+  algoliaDocSearch: () => {
+    console.log("docsearch initalised");
+    docsearch({
+      apiKey: "fc0011c6a297ab1c2c3e1d9d60f02d33",
+      indexName: "panaetius_blog_docsearch",
+      appId: "NQ5PE1DPPX",
+      inputSelector: "#autocomplete",
+      // debug: true,
+    });
+  },
+};
