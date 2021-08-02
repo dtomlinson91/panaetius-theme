352193290efef536b2617a6c530209c35221e577
Daniel Tomlinson <dtomlinson@panaetius.co.uk>
2021-08-02 03:14:26 +0100

    moving bootstrap + jquery to app.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,16 @@
 import "./scss/styles.scss";
 
-import(/* webpackChunkName: "jQuery"*/ "jquery");
-import(/* webpackChunkName: "bootstrap" */ "bootstrap");
 
 window.addEventListener("DOMContentLoaded", async (event) => {
   const { default: App } = await import(/* webpackChunkName: "app" */ "./js/App");
+
   App.bootstrapify();
   App.lazyload();
   App.loadFontAwesome();
-  // App.navbarFade();
   App.aos();
   App.scrollBar();
   App.tocbot();
-  // App.test();
   App.algoliaDocSearch();
+  // App.test();
+  // App.navbarFade();
 });
